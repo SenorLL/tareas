08f835e84b63d7ddda4bef7a8cb9e874fa4bde97
SenorLL <alanrodriguez4334@gmail.com>
2025-03-26 00:37:14 -0600

    arregle un pequeño error en el bobble sort

--- src/codigo_20.py
+++ src/codigo_20.py
@@ -1,13 +1,11 @@
 #bobble sort
-nums = [8,9,5,3,7,5,6,8,3,2,1]
+nums = [9,8,5,3,7,5,6,8,3,2,1]
 for i in range(len(nums)):
     for j in range(len(nums)):
-        if nums[i] < nums[j]:
-            ant = nums[i]
-            nums[i] = nums[j]
-            nums[j] = ant
-            print(nums[i], nums[j])
-            print(nums)
+        if nums[j] > nums[i]:
+            ant = nums[j]
+            nums[j] = nums[i]
+            nums[i] = ant
 print(nums)
 
     
